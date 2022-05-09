14dc35c4dc1b534f74634ff36743e189cf7eb787
Alexey Grishin <alexey.grishin@gmail.com>
2022-05-09 18:06:15 +0300

    add maven plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.20"
+    `maven-publish`
 }
 
-group = "io.github.kotlinq"
+group = "io.github.ko-linq"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -21,4 +22,16 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+publishing {
+    publications {
+        withType<MavenPublication> {
+            groupId = "io.github.ko-linq"
+            artifactId = "kotlinq-core"
+            version = "0.1"
+
+            from(components.getByName("java"))
+        }
+   }
 }
